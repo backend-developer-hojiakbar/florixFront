3327ef0b20b8f305553b4e8898541d48a796a2f1
backend-developer-hojiakbar <soyibnazarovhoji@gmail.com>
2025-06-13 19:48:32 +0500

    refactor: switch to webpack for better Node.js compatibility

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/, // Match TypeScript files
+        exclude: /node_modules/, // Exclude node_modules directory
+        use: {
+          loader: 'ts-loader', // Use ts-loader for TypeScript files
+          options: {
+            transpileOnly: true, // Only transpile TypeScript to JavaScript
+          },
+        },
+      },
+      {
+        test: /\.(css|scss)$/, // Match CSS and SCSS files
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // Match image files
+        type: 'asset/resource', // Use asset/resource type
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Use index.html as template
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
